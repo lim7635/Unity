df3eb460d01a12cbdd29a89eab8b6d572b148cb9
lim7635 <ju7635@naver.com>
2024-05-10 21:44:49 +0900

    2024.05.10 스크립트 생성 및 사용

---
+++ Program/Assets/Class2th (Export Package)/Scripts/Data.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Data : MonoBehaviour
+{
+    public int data = 25;
+
+    public void Start()
+    {
+        Debug.Log(data);
+    }
+
+}
