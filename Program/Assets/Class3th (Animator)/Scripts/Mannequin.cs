afe762cb7a48918bd83b54635cab82d2d83a140a
lim7635 <ju7635@naver.com>
2024-05-13 21:46:04 +0900

    2024.05.13 애니메이터 컨트롤러

---
+++ Program/Assets/Class3th (Animator)/Scripts/Mannequin.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mannequin : MonoBehaviour
+{
+    // 값 타입 : byte, char, bool, short, int
+    // long, float, double, decimal, struct 등
+
+    // 참조 타입 : class, array, collection, delegate(함수 포인터),
+    // interface, abstractclass(추상 클래스), event 등
+
+    // object : 값 & 참조 타입 모두 사용할 수 있는 자료형(참조 타입)
+}
