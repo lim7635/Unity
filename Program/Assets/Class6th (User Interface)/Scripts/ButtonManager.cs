8db3f15ad2689bef6c18a76527820f8bcb9b9bc3
lim7635 <ju7635@naver.com>
2024-05-17 21:45:37 +0900

    2024.05.17 유저 인터페이스

---
+++ Program/Assets/Class6th (User Interface)/Scripts/ButtonManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour
+{
+    public Text[] buttonText;
+    private string[] buttonName = { "START", "LOAD", "QUIT" };
+
+    private void Awake()
+    {
+        for(int i = 0; i < buttonName.Length; i++)
+        {
+            buttonText[i].text = buttonName[i];
+        }
+    }
+
+    public void GameStart()
+    {
+        Debug.Log("Start");
+    }
+
+    public void GameLoad()
+    {
+        Debug.Log("Load");
+    }
+
+    public void GameQuit()
+    {
+        Debug.Log("Quit");
+    }
+}
