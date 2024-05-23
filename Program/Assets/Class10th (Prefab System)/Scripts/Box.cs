3c4e191310b50abf87c5823f2062eddef987240f
lim7635 <ju7635@naver.com>
2024-05-23 21:42:58 +0900

    2024.05.23 프리팹 시스템

---
+++ Program/Assets/Class10th (Prefab System)/Scripts/Box.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    public int x = 1;
+    public int y = 1;
+    public int z = 1;
+
+    void Update()
+    {
+        transform.Rotate(x * Time.deltaTime, y * Time.deltaTime, z * Time.deltaTime);
+    }
+}
--- Program/Assets/Class9th (Instantiate & Destory)/Scripts/CreateManager.cs
+++ Program/Assets/Class9th (Instantiate & Destory)/Scripts/CreateManager.cs
@@ -13,6 +13,15 @@ public class CreateManager : MonoBehaviour
 
     void Start()
     {
+        // Invoke() : 특정한 시간마다 함수를 호출하는 함수입니다.
 
+        InvokeRepeating(nameof(Use), 1, 5); // 1초 뒤 생성 후 5초 마다 생성 반복
+    }
+
+    public void Use()
+    {
+        int select = Random.Range(0, 2);
+
+        factory.Create((UnitType)select);
     }
 }
